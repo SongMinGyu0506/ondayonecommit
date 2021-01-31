37f3400e010533194a70e8826c5c255a56402e6a
SongMinGyu0506 <ggp04114@naver.com>
2021-02-01 02:34:45 +0900

    temp

--- Algorithm/1712.cpp
+++ Algorithm/1712.cpp
@@ -3,22 +3,12 @@
 using namespace std;
 
 void solution(int A, int B, int C) {
-    if(B > C) {
-        cout << "-1";
+    if(B >= C) {
+        cout << "-1" << endl;
     }
     else {
-        int counter = 1;
-        int temp_B = B;
-        int temp_C = C;
-        while(true) {
-            if(A+B<C){
-                break;
-            }
-            counter++;
-            B=B+temp_B;
-            C=C+temp_C;
-        }
-        cout << counter;
+        int result = A/(C-B)+1;
+        cout << result << endl;
     }
 }
 
